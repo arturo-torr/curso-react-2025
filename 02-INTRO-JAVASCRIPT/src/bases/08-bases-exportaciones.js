1750d675c7edfe7651d26fac4ef95ca7b84e7b8c
Arturo <arturotorresorovio@gmail.com>
2025-01-20 18:44:44 +0100

    añadidas bases - promesas

--- 02-INTRO-JAVASCRIPT/src/bases/08-bases-exportaciones.js
+++ 02-INTRO-JAVASCRIPT/src/bases/08-bases-exportaciones.js
@@ -1,11 +1,11 @@
 // Import, export, funciones comunes
 import heroes, { owners } from "./data/heroes.js";
 
-const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
-const getHeroesByOwner = (owner) =>
+export const getHeroesByOwner = (owner) =>
   heroes.filter((heroe) => heroe.owner === owner);
 
 const hero = getHeroeById(1);
-console.log(getHeroeById(2));
-console.log(getHeroesByOwner("Marvel"));
+// console.log(getHeroeById(2));
+// console.log(getHeroesByOwner("Marvel"));
